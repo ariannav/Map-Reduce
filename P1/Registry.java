ff4c026bf8ab8926acf956f2ab4fa91f6075fcc2
Ari Vacca <ariannav14@gmail.com>
2018-01-18 21:18:03 -0700

    Not working with editing

--- P1/Registry.java
+++ P1/Registry.java
@@ -1,6 +1,33 @@
 package cs455.overlay;
 
+public class Registry implements Runnable{
 
-public static main(char[] argv){
-        
+    private ServerSocket sockit;
+
+    public static void main(String[] argv){
+        //Find available port. Create server socket.
+        createServerSocket();
+        System.out.println("Server socket ")
+        //Create foreground thread.
+
+    }
+
+    private void createServerSocket() throws IOException{
+        for(int i = 2000; i < 10000; i++){
+            try{
+                sockit = new ServerSocket(i);
+                return;
+            }
+            catch(Exception e){
+                continue;
+            }
+        }
+    }
+
+    private void run(){
+        //Foreground thread.
+    }
+
+    fuck you intellij;
+
 }
